aef3dcf52c19d5e7c2a6117717729d3daa2720c3
Johan <johan@ftm.nl>
2023-12-06 16:45:09 +0100

    use subgrids, menu styles

--- ui/src/routes/+layout.svelte
+++ ui/src/routes/+layout.svelte
@@ -1,32 +1,26 @@
+<script>
+    import Navigation from "./Navigation.svelte";
+</script>
+
 <div class="container">
+    <nav>
+        <Navigation />
+    </nav>
+
     <header>
-        <input type="text" placeholder="search">
+        <ul>
+            <li><a href="/">Link</a></li>
+            <li><a href="/">Link</a></li>
+            <li><a href="/">Link</a></li>
+            <li><a href="/">Link</a></li>
+        </ul>
         
         <p>Username</p>
     </header>
 
-    <nav>
-        <header>
-            <h1>Insight</h1>
-        </header>
-
-        <section>
-            <ul>
-                <li><a href="/">Link</a></li>
-                <li><a href="/">Link</a></li>
-                <li><a href="/">Link</a></li>
-                <li><a href="/">Link</a></li>
-            </ul>
-        </section>
-    </nav>
-
     <main>
         <slot />
     </main>
-
-    <aside>
-        <h2>Chat</h2>
-    </aside>
 
     <footer>
         <h3>Footer</h3>
@@ -35,36 +29,27 @@
 
 
 <style>
-    :root {
-        --header-height: 5rem;
-        --gap: 2.5rem;
-        --half-gap: 1.25rem;
-    }
     .container {
         display: grid;
 
         grid-template-areas:
-            "nav header header"
-            "nav content side"
-            "nav footer footer";
+            "nav header"
+            "nav content"
+            "nav footer";
 
-        grid-template-columns: 1fr 3fr 2fr;
-        grid-template-rows: auto 1fr auto;
-        gap: var(--gap);
+        grid-template-columns: 1fr 4fr;
+        grid-template-rows: 8rem 1fr 8rem;
 
         height: 100vh;
     }
 
     header {
-        padding-right: var(--gap);
         grid-area: header;
         display: grid;
-        grid-template-columns: 1fr 1fr 1fr;
-        height: var(--header-height);
+        grid-template-columns: 3fr 1fr;
         align-items: center;
     }
     header p {
-        grid-column: 3 / 4;
         text-align: right;
     }
 
@@ -72,44 +57,18 @@
         grid-area: nav;
         background: #494582;
         color: #F9FAFB;
-    }
-
-    nav header {
-        height: var(--header-height);
-        border-bottom: 1px solid #38325D;
         display: grid;
-        align-items: center;
-    }
-
-    nav header h1 {
-        font-size: 1.5rem;
-        padding: 0 var(--half-gap);
-    }
-
-    nav section {
-        border-top: 1px solid #5756A6;
-    }
-
-    nav section ul {
-        padding: 1rem 0;
-    }
-
-    nav section ul a {
-        display: block;
-        padding: 0.5rem var(--half-gap);
-        color: inherit;
+        grid-template-rows: subgrid;
+        box-shadow: -1rem 0 1rem -1rem #38325D inset;
     }
 
     main {
         grid-area: content;
     }
 
-    aside {
-        grid-area: side;
-    }
-
     footer {
         grid-area: footer;
+        display: grid;
+        align-items: center;
     }
-
 </style>
---
+++ ui/src/routes/Navigation.svelte
@@ -0,0 +1,41 @@
+<header>
+    <h1>Insight</h1>
+</header>
+
+<section>
+    <ul>
+        <li><a href="/">Link</a></li>
+        <li><a href="/">Link</a></li>
+        <li><a href="/">Link</a></li>
+        <li><a href="/">Link</a></li>
+    </ul>
+</section>
+
+<style>
+    header {
+        border-bottom: 1px solid #38325D;
+        display: grid;
+        align-items: center;
+    }
+
+    header h1 {
+        font-size: 1.5rem;
+        color: inherit;
+        padding: 0 var(--gap);
+    }
+
+    ul {
+        padding: 0;
+        margin: 0;
+    }
+
+    a {
+        display: block;
+        color: inherit;
+        border-top: 1px solid #5756A6;
+        border-bottom: 1px solid #38325D;
+        padding: 1.5rem var(--gap);
+        text-decoration: none;
+        font-weight: bold;
+    }
+</style>
