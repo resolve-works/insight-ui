20d253b7b4ba21ebec33a78ab90d9270b2ec2b53
Johan <johan@ftm.nl>
2023-12-06 03:31:52 +0100

    add sass, add start of layout

---
+++ ui/src/routes/+layout.svelte
@@ -0,0 +1,115 @@
+<div class="container">
+    <header>
+        <input type="text" placeholder="search">
+        
+        <p>Username</p>
+    </header>
+
+    <nav>
+        <header>
+            <h1>Insight</h1>
+        </header>
+
+        <section>
+            <ul>
+                <li><a href="/">Link</a></li>
+                <li><a href="/">Link</a></li>
+                <li><a href="/">Link</a></li>
+                <li><a href="/">Link</a></li>
+            </ul>
+        </section>
+    </nav>
+
+    <main>
+        <slot />
+    </main>
+
+    <aside>
+        <h2>Chat</h2>
+    </aside>
+
+    <footer>
+        <h3>Footer</h3>
+    </footer>
+</div>
+
+
+<style lang="scss">
+    $header-height: 5rem;
+    $gap: 2.5rem;
+
+    .container {
+        display: grid;
+
+        grid-template-areas:
+            "nav header header"
+            "nav content side"
+            "nav footer footer";
+
+        grid-template-columns: 1fr 3fr 2fr;
+        grid-template-rows: auto 1fr auto;
+        gap: $gap;
+
+        height: 100vh;
+    }
+
+    header {
+        padding-right: $gap;
+        grid-area: header;
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        height: $header-height;
+        align-items: center;
+        
+        p {
+            grid-column: 3 / 4;
+            text-align: right;
+        }
+    }
+
+    nav {
+        grid-area: nav;
+        background: #494582;
+        color: #F9FAFB;
+
+
+        header {
+            height: $header-height;
+            border-bottom: 1px solid #38325D;
+            display: grid;
+            align-items: center;
+
+            h1 {
+                font-size: 1.5rem;
+                padding: 0 $gap / 2;
+            }
+        }
+
+        section {
+            border-top: 1px solid #5756A6;
+
+            ul {
+                padding: 1rem 0;
+
+                a {
+                    display: block;
+                    padding: 0.5rem $gap / 2;
+                    color: inherit;
+                }
+            }
+        }
+    }
+
+    main {
+        grid-area: content;
+    }
+
+    aside {
+        grid-area: side;
+    }
+
+    footer {
+        grid-area: footer;
+    }
+
+</style>
--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to Insight</h1>
+<p>The search engine for FOIA requests.</p>
