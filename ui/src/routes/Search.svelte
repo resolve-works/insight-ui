fd12a5d28677481ee8dec788576f119688be4d76
Johan <johan@ftm.nl>
2023-12-07 02:37:27 +0100

    add custom fonts

--- ui/src/routes/Search.svelte
+++ ui/src/routes/Search.svelte
@@ -1,13 +1,19 @@
+<script>
+    import Icon from './Icon.svelte';
+</script>
+
 <form method="get">
     <input name="query" type="text" placeholder="Kwartiermaker" value="Kwartiermaker" />
 
-    <input type="submit" value="🔍">
+    <button>
+        <Icon class="gg-search" />
+    </button>
 </form>
 
 <style>
     form {
         display: grid;
-        grid-template-columns: 5fr 4em;
+        grid-template-columns: 5fr 3rem;
     }
 
     input[type=text] {
@@ -15,10 +21,12 @@
         grid-row: 1;
     }
 
-    input[type=submit] {
+    button {
         grid-column: 2 / 3;
         grid-row: 1;
         background: none;
+        padding: 0 0.5rem;
+        color: var(--text-color-dark);
         border: none;
         cursor: pointer;
     }
