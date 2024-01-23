c121efb883dc7f91b5cc12f6948c672ad6ad988e
Johan <johan@ftm.nl>
2024-01-23 13:32:21 +0100

    make selection and dropping of to be uploaded files work

--- ui/src/routes/files/Uploader.svelte
+++ ui/src/routes/files/Uploader.svelte
@@ -2,21 +2,41 @@
 <script lang=ts>
     import Icon from '../Icon.svelte';
 
-    let input: HTMLInputElement;
+    let is_dragover = false;
 
-    function upload(e: Event) {
-        for(const pagestream of e.target.files) {
-            console.log(pagestream)
-            // TODO - POST pagestream
+    let input: HTMLInputElement;
+    let files: File[];
+    $: files = [];
+
+    function drop(e: DragEvent) {
+        if( ! e.dataTransfer) {
+            return
+        }
+
+        for(const file of e.dataTransfer.files) {
+            input.files.push(file)
+        }
+    }
+
+    function upload() {
+        if( ! input.files ) {
+            return;
+        }
+
+        for(const file of input.files) {
+            files = [...files, file]
+            // TODO - Create pagestream in DB
             // TODO - Get signed write url
             // TODO - POST actual file to S3 signed url
         }
+
+        input.value = '';
     }
 </script>
 
 <input type="file" accept=".pdf" multiple bind:this={input} on:change={upload} />
 
-<button on:click={() => input.click()}>
+<button on:click={() => input.click()} on:drop|preventDefault={drop} on:dragover|preventDefault on:dragenter|preventDefault={() => is_dragover = true} on:dragexit|preventDefault={() => is_dragover = false} class:dragover={is_dragover}>
     <span>
         <Icon class="gg-software-upload" />
     </span>
@@ -24,7 +44,16 @@
     <h2>Drop files here to upload</h2>
 </button>
 
+
+{#each files as file}
+    <div>{file.name}</div>
+{/each}
+
 <style>
+    input {
+        display: none;
+    }
+
     button {
         padding: 1.5rem;
         margin-bottom: 1rem;
@@ -38,6 +67,7 @@
         width: 100%;
     }
 
+    button.dragover,
     button:hover {
         color: var(--color-primary);
         cursor: pointer;
