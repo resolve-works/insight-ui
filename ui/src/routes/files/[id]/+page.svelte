e0e5b83ead54ea152a5bdae15eaff6911756e86d
Johan <johan@ftm.nl>
2024-01-30 22:40:19 +0100

    start of embedded documents

--- ui/src/routes/files/[id]/+page.svelte
+++ ui/src/routes/files/[id]/+page.svelte
@@ -1,9 +1,11 @@
 
 <script>
     import Page from '$lib/Page.svelte';
-    import Meta from './Meta.svelte';
     import PDFViewer from '$lib/PDFViewer.svelte';
+    import Embed from './Embed.svelte';
     export let data;
+
+    const { documents } = data.file;
 </script>
 
 <Page class="with-sidebar-right">
@@ -11,7 +13,13 @@
 </Page>
 
 <aside>
-    <Meta {...data.file} />
+    <header>
+        <h2>{documents.length} embedded document{documents.length == 1 ? '' : 's'}</h2>
+    </header>
+
+    {#each documents as document, index}
+        <Embed {...document} />
+    {/each}
 </aside>
 
 <style>
@@ -22,5 +30,10 @@
         grid-template-rows: subgrid;
         padding: 0 var(--padding-x);
         color: var(--text-color-light);
+    }
+
+    header {
+        display: grid;
+        align-items: center;
     }
 </style>
---
+++ ui/src/routes/files/[id]/Embed.svelte
@@ -0,0 +1,23 @@
+
+<script lang="ts">
+    export let from_page: number;
+    export let to_page: number;
+
+    export let id: string;
+    export let name: string;
+</script>
+
+<h3>{name}</h3>
+
+<form>
+    from: <input type="number" value={from_page + 1} />
+    to:<input type="number" value={to_page} />
+</form>
+
+<style>
+    h3 {
+        overflow-x: hidden;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+    }
+</style>
