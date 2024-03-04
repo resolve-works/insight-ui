550be1dcd2c17ecc2bba32582ef37f0d35a52dc2
Johan <johan@ftm.nl>
2024-03-04 20:08:27 +0100

    Assert queue on event socket open, and consume messages

--- ui/src/routes/events/+server.ts
+++ ui/src/routes/events/+server.ts
@@ -3,16 +3,28 @@ import amqplib from 'amqplib';
 
 export async function GET({ locals }) {
     const password = `${encodeURIComponent(locals.access_token)}`
-    const conn = await amqplib.connect(`amqp://username:${password}@rabbitmq`);
+    const connection = await amqplib.connect(`amqp://username:${password}@rabbitmq`);
 
-    const channel = await conn.createChannel();
+    const queue = 'test'
 
+    const channel = await connection.createChannel();
+    await channel.assertQueue(queue, { autoDelete: true });
+
+    // Forward rabbitmq messages for user
 	const readable = new ReadableStream({
 		start(ctr) {
-			this.interval = setInterval(() => ctr.enqueue(`data: ${new Date().toISOString()}\n\n`), 1000);
+            channel.consume(queue, (message) => {
+                if (message !== null) {
+                    console.log(`Received: ${message.content.toString()}`);
+                    ctr.enqueue(`${message.content.toString()}\n\n`)
+                    channel.ack(message);
+                } else {
+                    console.log('Consumer cancelled by server');
+                }
+            });
 		},
 		cancel() {
-			clearInterval(this.interval);
+            connection.close()
 		}
 	});
 
