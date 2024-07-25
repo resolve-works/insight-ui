2403d28b60788a4157c245fd0ea5fd56da0f5878
Johan Schuijt <johan.schuijt@ftm.nl>
2024-07-25 23:40:33 +0200

    Support folders in search form, disable animations of multiselect

--- src/lib/Search.svelte
+++ src/lib/Search.svelte
@@ -1,34 +1,39 @@
 <script lang="ts">
-    import Icon from '$lib/Icon.svelte';
-    export let value = '';
+	import { page } from '$app/stores';
+	import Icon from '$lib/Icon.svelte';
+	export let value = '';
 </script>
 
 <form method="get" action="/search">
-    <input name="query" type="text" placeholder="Search contents by keyword" value={value} />
+	<input name="query" type="text" placeholder="Search contents by keyword" {value} />
 
-    <button>
-        <Icon class="gg-search" />
-    </button>
+	{#each $page.url.searchParams.getAll('folder') as folder}
+		<input type="hidden" name="folder" value={folder} />
+	{/each}
+
+	<button>
+		<Icon class="gg-search" />
+	</button>
 </form>
 
 <style>
-    form {
-        display: grid;
-        grid-template-columns: 5fr 3rem;
-    }
+	form {
+		display: grid;
+		grid-template-columns: 5fr 3rem;
+	}
 
-    input[type=text] {
-        grid-column: 1 / 3;
-        grid-row: 1;
-    }
+	input[type='text'] {
+		grid-column: 1 / 3;
+		grid-row: 1;
+	}
 
-    button {
-        grid-column: 2 / 3;
-        grid-row: 1;
-        background: none;
-        padding: 0 0.5rem;
-        color: var(--text-color-dark);
-        border: none;
-        cursor: pointer;
-    }
+	button {
+		grid-column: 2 / 3;
+		grid-row: 1;
+		background: none;
+		padding: 0 0.5rem;
+		color: var(--text-color-dark);
+		border: none;
+		cursor: pointer;
+	}
 </style>
