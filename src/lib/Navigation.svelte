1808bb9966e0d6e1c5fe3bac03bd8f043e4b175a
Johan Schuijt <johan.schuijt@ftm.nl>
2024-07-28 18:42:36 +0200

    spend to much time on styling

--- src/lib/Navigation.svelte
+++ src/lib/Navigation.svelte
@@ -1,80 +1,83 @@
-<script lang=ts>
-    import { page } from '$app/stores';
-    import Icon from '$lib/Icon.svelte';
+<script lang="ts">
+	import { page } from '$app/stores';
+	import Icon from '$lib/Icon.svelte';
 
-    const items = [
-        { href: "/search", name: "Search", class: "gg-search" },
-        { href: "/conversations", name: "Conversations", class: "gg-comment" },
-        { href: "/files", name: "Files", class: "gg-folder"},
-    ]
+	const items = [
+		{ href: '/search', name: 'Search', class: 'gg-search' },
+		{ href: '/conversations', name: 'Conversations', class: 'gg-comment' },
+		{ href: '/files', name: 'Files', class: 'gg-folder' }
+	];
 </script>
 
 <a class="logo" href="/">
-    <h1>Insight</h1>
-    <Icon class="gg-menu" />
+	<h1>Insight</h1>
+	<Icon class="gg-menu" />
 </a>
 
 <ul>
-    {#each items as item}
-        <li>
-            <a href={item.href} class:active={$page.url.pathname.startsWith(new URL(item.href, $page.url.origin).pathname)}>
-                <span>{item.name}</span>
-                <Icon class={item.class} />
-            </a>
-        </li>
-    {/each}
+	{#each items as item}
+		<li>
+			<a
+				href={item.href}
+				class:active={$page.url.pathname.startsWith(new URL(item.href, $page.url.origin).pathname)}
+			>
+				<span>{item.name}</span>
+				<Icon class={item.class} />
+			</a>
+		</li>
+	{/each}
 </ul>
 
 <form method="POST" action="/auth/logout">
-    <button>
-        <span>Log out</span>
-        <Icon class="gg-log-out" />
-    </button>
+	<button>
+		<span>Log out</span>
+		<Icon class="gg-log-out" />
+	</button>
 </form>
 
 <style>
-    button {
-        background: none;
-        border: none;
-        font-size: 1rem;
-        width: 100%;
-    }
+	button {
+		background: none;
+		border: none;
+		font-size: 1rem;
+		width: 100%;
+	}
 
-    a,
-    button {
-        grid-template-columns: auto auto;
-        justify-content: space-between;
-        display: grid;
-        align-items: center;
-        color: inherit;
-        text-decoration: none;
-        padding: 1.5rem var(--padding-x);
-    }
+	a,
+	button {
+		grid-template-columns: auto auto;
+		justify-content: space-between;
+		display: grid;
+		align-items: center;
+		color: inherit;
+		text-decoration: none;
+		padding: 1.5rem var(--padding-x);
+	}
 
-    a.active {
-        background: var(--color-subnavigation);
-        border-top: 1px solid transparent;
-        border-bottom: 1px solid var(--color-navigation-darker);
-    }
+	a.active {
+		background: var(--color-subnavigation);
+		border-top: 1px solid transparent;
+		border-bottom: 1px solid var(--color-navigation-darker);
+	}
 
-    a.logo {
-        border-bottom: 1px solid var(--color-navigation-darker);
-    }
-    a.logo h1 {
-        font-size: 1.5rem;
-        margin: 0;
-    }
+	a.logo {
+		border-bottom: 1px solid var(--color-navigation-darker);
+	}
+	a.logo h1 {
+		font-size: 1.5rem;
+		margin: 0;
+	}
 
-    ul {
-        padding: 0;
-        margin: 0;
-    }
-    li a {
-        border-top: 1px solid var(--color-navigation-lighter);
-        border-bottom: 1px solid var(--color-navigation-darker);
-    }
+	ul {
+		padding: 0;
+		margin: 0;
+	}
+	li a {
+		border-top: 1px solid var(--color-navigation-lighter);
+		border-bottom: 1px solid var(--color-navigation-darker);
+	}
 
-    form > button {
-        height: 100%;
-    }
+	form > button {
+		height: 100%;
+	}
 </style>
--- src/lib/SideBar.svelte
+++ src/lib/SideBar.svelte
@@ -1,32 +1,33 @@
 <aside>
-    <header>
-        <slot name="header" />
-    </header>
+	<header>
+		<slot name="header" />
+	</header>
 
-    <slot />
+	<slot />
 </aside>
 
 <style>
-    aside {
-        padding: 0 var(--padding-x);
-        background: var(--color-subnavigation);
-        color: var(--text-color-light);
-        box-shadow: var(--box-shadow);
-        display: grid;
-        grid-template-rows: subgrid;
-        grid-column: 2 / 3;
-        grid-row: 1 / 4;
-        position: sticky;
-        top: 0;
-        min-height: 100vh;
-    }
+	aside {
+		padding: 0 var(--padding-x);
+		background: var(--color-subnavigation);
+		color: var(--text-color-light);
+		box-shadow: var(--box-shadow);
+		display: grid;
+		grid-template-rows: subgrid;
+		grid-column: 2 / 3;
+		grid-row: 1 / 4;
+		position: sticky;
+		top: 0;
+		min-height: 100vh;
+	}
 
-    header {
-        display: grid;
-        align-items: center;
-    }
-    
-    header :global(input[type=text]) {
-        border-color: var(--color-subnavigation-darker);
-    }
+	header {
+		display: grid;
+		align-items: center;
+	}
+
+	aside :global() {
+		--input-border-color: var(--color-primary-darker);
+		--sms-border: var(--input-border-size) solid var(--input-border-color);
+	}
 </style>
