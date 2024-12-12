055a6f41816280be2a5c5c6c85746f7462843663
Johan Schuijt <johan.schuijt@ftm.nl>
2024-12-12 18:53:17 +0100

    flatten folder tree, add focus, add keyboard navigation

--- src/lib/FolderFilter.svelte
+++ src/lib/FolderFilter.svelte
@@ -1,8 +1,14 @@
 <script context="module" lang="ts">
 	export type Option = { key: string; doc_count: number };
 
+	export type Inode = Option & {
+		label: string;
+		children: Inode[];
+	};
+
 	export type Folder = Option & {
-		children: Folder[];
+		label: string;
+		indent: number;
 	};
 </script>
 
@@ -11,15 +17,14 @@
 	import { browser } from '$app/environment';
 	import { createEventDispatcher, tick } from 'svelte';
 	import FolderFilterFolder from './FolderFilterFolder.svelte';
+	import { parse_folders } from './search';
 
-	let folders_param = $page.url.searchParams.get('folders');
-
-	let selected = folders_param ? JSON.parse(folders_param) : [];
+	let selected = parse_folders($page.url.searchParams.get('folders'));
 	let query = '';
 	let loading = false;
 	let is_opened = true;
-	let options: Option[] = [];
 	let folders: Folder[] = [];
+	let focussed_index = 0;
 
 	const dispatch = createEventDispatcher();
 
@@ -28,92 +33,124 @@
 		dispatch('change');
 	}
 
-	function sort(folders: Folder[]) {
-		folders.sort((a: Folder, b: Folder) => b.doc_count - a.doc_count);
-		folders.forEach((folder) => sort(folder.children));
-		return folders;
+	// Sort tree by doc_count
+	function sort(inodes: Inode[]) {
+		inodes.sort((a: Inode, b: Inode) => b.doc_count - a.doc_count);
+		inodes.forEach((Inode) => sort(Inode.children));
+		return inodes;
 	}
 
 	async function fetch_folders(query: string) {
-		loading = true;
 		// perform some fetch/database request here to get list of options matching query
 		const response = await fetch(`/search/folders?query=${query}`);
 		const options = await response.json();
 
 		// Build a tree from the paths
-		const unsorted_folders = options.reduce((folders: Folder[], option: Option) => {
+		return options.reduce((inodes: Inode[], option: Option) => {
 			const levels = option.key.split('/').filter(Boolean);
-			let target = folders;
+			let target = inodes;
 
-			levels.forEach((level, index) => {
+			levels.forEach((label, index) => {
 				const is_leaf = index == levels.length - 1;
 				if (!is_leaf) {
-					target = target.find((child) => child.key == level)!.children;
+					target = target.find((child) => child.label == label)!.children;
 				} else {
-					target.push({ ...option, key: level, children: [] });
+					target.push({ ...option, label, children: [] });
 				}
 			});
 
-			return folders;
+			return inodes;
 		}, []);
-
-		// Sort tree by doc_count
-		folders = sort(unsorted_folders);
-
-		loading = false;
 	}
 
-	function open() {
-		is_opened = true;
+	function flatten_inode_tree(inodes: Inode[], indent = 0): Folder[] {
+		return inodes
+			.map((inode) => {
+				const { label, key, doc_count } = inode;
+				return [
+					{ label, key, indent, doc_count },
+					...flatten_inode_tree(inode.children, indent + 1)
+				];
+			})
+			.flat();
 	}
 
-	function close() {
-		is_opened = false;
+	function keydown(event: KeyboardEvent) {
+		switch (event.key) {
+			case 'ArrowUp':
+				event.preventDefault();
+				focussed_index -= 1;
+				break;
+			case 'ArrowDown':
+				event.preventDefault();
+				focussed_index += 1;
+				break;
+			case 'Enter':
+				event.preventDefault();
+				selected = [...selected, folders[focussed_index].key];
+		}
 	}
 
 	$: {
 		if (browser) {
-			fetch_folders(query);
+			(async () => {
+				loading = true;
+				const unsorted_folders = await fetch_folders(query);
+				folders = flatten_inode_tree(sort(unsorted_folders));
+				focussed_index = 0;
+				loading = false;
+			})();
 		}
 	}
 </script>
 
-<div class:is-opened={is_opened}>
-	<input type="hidden" name="folders" value={JSON.stringify(selected)} />
+<input type="hidden" name="folders" value={JSON.stringify(selected)} />
 
-	<input type="text" bind:value={query} on:focus={open} on:blur={close} />
+<input
+	class:is-opened={is_opened}
+	type="text"
+	bind:value={query}
+	on:focus={() => (is_opened = true)}
+	on:blur={() => (is_opened = false)}
+	on:keydown={keydown}
+/>
 
-	<ul class="folders">
-		{#each folders as folder}
-			<FolderFilterFolder {...folder} />
+<div class="holder">
+	<ul class:is-opened={is_opened} class="folders">
+		{#each folders as folder, index}
+			<FolderFilterFolder
+				{...folder}
+				is_selected={index == focussed_index}
+				on:focus={() => (focussed_index = index)}
+			/>
 		{/each}
 	</ul>
 </div>
 
 <style>
-	div {
-		position: relative;
-	}
-
 	input[type='text'] {
 		position: relative;
-		display: block;
+		width: 100%;
 	}
 
-	.is-opened input[type='text'] {
+	input[type='text'].is-opened {
 		border-bottom-color: var(--input-background-color);
 		border-bottom-left-radius: 0;
 		border-bottom-right-radius: 0;
 		z-index: 2;
 	}
 
-	.folders {
+	.holder {
 		position: relative;
+	}
+
+	.folders {
+		position: absolute;
 		top: calc(var(--input-border-size) * -1);
 		display: none;
 		margin: 0;
 		color: var(--text-color-dark);
-		padding: 0.5rem 1rem;
+		padding: 0;
 		list-style-type: none;
 		max-width: 40rem;
 		max-height: 20rem;
@@ -126,7 +163,7 @@
 		z-index: 1;
 	}
 
-	.is-opened .folders {
+	.folders.is-opened {
 		display: block;
 	}
 </style>
--- src/lib/FolderFilterFolder.svelte
+++ src/lib/FolderFilterFolder.svelte
@@ -1,33 +1,39 @@
 <script lang="ts">
-	import type { Folder } from './FolderFilter.svelte';
 	import Icon from './Icon.svelte';
+	import { createEventDispatcher } from 'svelte';
 
-	export let key: string;
+	const dispatch = createEventDispatcher();
+
+	export let label: string;
 	export let doc_count: number;
-	export let children: Folder[];
-
 	export let indent = 0;
+	export let is_selected = false;
+
 	const padding = indent > 0 ? (indent - 1) * 2 + 1 : indent;
 </script>
 
-<li style="padding-left: {padding}rem">
+<li
+	style="padding-left: {padding + 1}rem"
+	class:is_selected
+	on:mouseenter={() => dispatch('focus')}
+>
 	{#if indent > 0}
 		<Icon class="gg-corner-down-right" />
 	{/if}
-	<span class="name" class:padded={indent > 0}>{key}</span>
+	<span class="name" class:padded={indent > 0}>{label}</span>
 	<span>{doc_count} file{doc_count != 1 ? 's' : ''}</span>
 </li>
-
-{#each children as child}
-	<svelte:self {...child} indent={indent + 1} />
-{/each}
 
 <style>
 	li {
 		display: flex;
 		align-items: center;
 		line-height: 1.6rem;
-		padding: 0.25rem 0;
+		padding: 0.25rem 1rem;
+	}
+
+	li.is_selected {
+		background: #ff0;
 	}
 
 	li :global(.icon) {
