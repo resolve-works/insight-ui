4164d3570a2156967cb3ea02cd1ad05a09a988cc
Johan Schuijt <johan.schuijt@ftm.nl>
2024-12-18 20:20:13 +0100

    implement pdfviewer using effects

--- src/lib/PDFViewer.svelte
+++ src/lib/PDFViewer.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
-	import { run } from 'svelte/legacy';
-
-	import { onMount, getContext } from 'svelte';
+	import { getContext } from 'svelte';
 	import * as pdfjs from 'pdfjs-dist';
 	import type { PDFWorker } from 'pdfjs-dist';
 	import 'pdfjs-dist/web/pdf_viewer.css';
@@ -12,95 +10,89 @@
 		highlights: string[] | undefined;
 	}
 
-	let { url, page_number, highlights }: Props = $props();
+	let { url, page_number, highlights = [] }: Props = $props();
 
-	let container: HTMLElement = $state();
-	let canvas: HTMLCanvasElement = $state();
-	let text: HTMLElement = $state();
-	let load_page: Function = $state();
-	let set_highlights: Function = $state();
+	let container: HTMLElement;
+	let canvas: HTMLCanvasElement;
+	let text: HTMLElement;
 
 	// Re-use existing worker so we don't load a new one every time we render a document
 	const worker: PDFWorker = getContext('pdfjs-worker');
 
-	run(() => {
-		if (load_page) {
-			load_page(page_number);
+	const pdf_promise = $derived(pdfjs.getDocument({ url, worker }).promise);
+	const page_promise = $derived(
+		pdf_promise
+			.then((pdf) => pdf.getPage(page_number))
+			.then((page) => {
+				const default_viewport = page.getViewport({ scale: 1 });
+				const scale = container.clientWidth / default_viewport.width;
+				const viewport = page.getViewport({ scale: scale });
+
+				return {
+					page,
+					scale,
+					viewport
+				};
+			})
+	);
+
+	$effect(async () => {
+		const { page, viewport } = await page_promise;
+		// Support HiDPI-screens.
+		const outputScale = window.devicePixelRatio || 1;
+
+		const context = canvas.getContext('2d');
+		if (!context) {
+			throw Error('Could not get rendering context');
 		}
+
+		canvas.width = Math.floor(viewport.width * outputScale);
+		canvas.height = Math.floor(viewport.height * outputScale);
+		canvas.style.width = Math.floor(viewport.width) + 'px';
+		canvas.style.height = Math.floor(viewport.height) + 'px';
+
+		const transform = outputScale !== 1 ? [outputScale, 0, 0, outputScale, 0, 0] : undefined;
+
+		page.render({ canvasContext: context, transform, viewport });
 	});
 
-	run(() => {
-		if (set_highlights) {
-			set_highlights(highlights ?? []);
+	$effect(async () => {
+		const { page, viewport, scale } = await page_promise;
+		text.innerHTML = '';
+		text.style.setProperty('--scale-factor', scale.toString());
+
+		const text_layer = new pdfjs.TextLayer({
+			textContentSource: await page.getTextContent(),
+			container: text,
+			viewport
+		});
+
+		// Try to highlight
+		if (text_layer) {
+			await text_layer.render();
 		}
-	});
 
-	onMount(async () => {
-		const pdf = await pdfjs.getDocument({ url, worker }).promise;
-
-		load_page = async (page_number: number) => {
-			const page = await pdf.getPage(page_number);
-
-			const default_viewport = page.getViewport({ scale: 1 });
-			const scale = container.clientWidth / default_viewport.width;
-			const viewport = page.getViewport({ scale: scale });
-			// Support HiDPI-screens.
-			const outputScale = window.devicePixelRatio || 1;
-
-			const context = canvas.getContext('2d');
-			if (!context) {
-				throw Error('Could not get rendering context');
+		for (let child of text.children) {
+			if (!child.textContent) {
+				continue;
 			}
 
-			canvas.width = Math.floor(viewport.width * outputScale);
-			canvas.height = Math.floor(viewport.height * outputScale);
-			canvas.style.width = Math.floor(viewport.width) + 'px';
-			canvas.style.height = Math.floor(viewport.height) + 'px';
+			let innerHTML = child.textContent;
 
-			const transform = outputScale !== 1 ? [outputScale, 0, 0, outputScale, 0, 0] : undefined;
-
-			page.render({ canvasContext: context, transform, viewport });
-
-			text.innerHTML = '';
-			text.style.setProperty('--scale-factor', scale.toString());
-
-			const text_layer = new pdfjs.TextLayer({
-				textContentSource: await page.getTextContent(),
-				container: text,
-				viewport
-			});
-
-			// Try to highlight
-			if (text_layer) {
-				await text_layer.render();
-			}
-			set_highlights(highlights ?? []);
-		};
-
-		// Find all words in the textLayer and wrap them in highlight
-		set_highlights = function (highlights: string[]) {
-			for (let child of text.children) {
-				if (!child.textContent) {
-					continue;
+			for (let highlight of highlights) {
+				const match = innerHTML.match(new RegExp(String.raw`\b${highlight}\b`, 'i'));
+				if (match) {
+					// Replace the word in the match to keep the original whitespace
+					// TODO - this could replace the "strong" tag itself
+					innerHTML = innerHTML.replace(
+						match[0],
+						match[0].replace(highlight, `<em class="highlight">${highlight}</em>`)
+					);
 				}
-
-				let innerHTML = child.textContent;
-
-				for (let highlight of highlights) {
-					const match = innerHTML.match(new RegExp(String.raw`\b${highlight}\b`, 'i'));
-					if (match) {
-						// Replace the word in the match to keep the original whitespace
-						// TODO - this could replace the "strong" tag itself
-						innerHTML = innerHTML.replace(
-							match[0],
-							match[0].replace(highlight, `<em class="highlight">${highlight}</em>`)
-						);
-					}
-				}
-
-				child.innerHTML = innerHTML;
 			}
-		};
+
+			child.innerHTML = innerHTML;
+		}
 	});
 </script>
 
